6961222299c358ebabfcb228b73a2b8506556e0a
mn-48 <nazmul.cse48@gmail.com>
2024-07-11 10:26:17 +0600

    modified:   c-sharp/producer/bin/Debug/net8.0/producer.dll         modified:   c-sharp/producer/bin/Debug/net8.0/producer.pdb         modified:   c-sharp/producer/obj/Debug/net8.0/producer.AssemblyInfo.cs         modified:   c-sharp/producer/obj/Debug/net8.0/producer.AssemblyInfoInputs.cache         modified:   c-sharp/producer/obj/Debug/net8.0/producer.dll         modified:   c-sharp/producer/obj/Debug/net8.0/producer.pdb         modified:   c-sharp/producer/obj/Debug/net8.0/producer.sourcelink.json         modified:   c-sharp/producer/obj/Debug/net8.0/ref/producer.dll         modified:   c-sharp/producer/obj/Debug/net8.0/refint/producer.dll

--- c-sharp/producer/obj/Debug/net8.0/producer.AssemblyInfo.cs
+++ c-sharp/producer/obj/Debug/net8.0/producer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aae00287c28e9e06cf5db18cf813d081ec20876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0fe6b8440a3449aed38072fe3c4bdc25b82068b")]
 [assembly: System.Reflection.AssemblyProductAttribute("producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
